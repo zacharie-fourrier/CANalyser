f1902d6400a81162f091080bc49c662a41bf9aac
HEELZacky <95706083+HEELZacky@users.noreply.github.com>
2022-12-02 10:51:25 +0100

    Started working on more command line args
    and text file support

--- CANanlyst.cpp
+++ CANanlyst.cpp
@@ -9,6 +9,7 @@ using namespace can;
 int main(int argc, char * argv[]) {
 
 	string frame;
+	bool ui = 0, frameValid = 0;
 
 	if (argv[1]) {
 		frame = argv[1];
@@ -16,15 +17,34 @@ int main(int argc, char * argv[]) {
 	else {
 		cout << "Binary CAN frame :" << endl;
 		cin >> frame;
+		ui = 1;
 	}
 
-	regex r (R"([^0,1]+)");
-	smatch m;
+	regex r1 (R"([0,1]+)");
+	smatch m1;
+	regex r2 (R"(.+\.txt)");
+	smatch m2;
 
-	if (regex_search(frame, m, r)) {
-		cout << "Invalid CAN frame format, please use a valid CAN frame." << endl;
-		return 1; //Invalid frame format
-	}
+	do {
+		if (regex_match(frame, m1, r1)) { //binary frame
+			frameValid = 1;
+
+		}
+		else if (regex_match(frame, m2, r2)) { //text file
+			frameValid = 1;
+
+		}
+		else { //neither a binary frame nor a text file
+			cout << "Invalid CAN frame format, please use a valid CAN frame." << endl;
+			if (!ui) {
+				return -1; //Invalid frame format error code
+			}
+			else { //ask for frame or text file again, only if not using command lines arguments
+				cout << "Binary CAN frame :" << endl;
+				cin >> frame;
+			}
+		}
+	} while (!frameValid);
 	
 	Frame CAN(frame);
 	cout << CAN.toString() << endl;
